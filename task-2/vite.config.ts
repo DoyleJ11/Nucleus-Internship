4482e12fb6480b8e142bebaa234c54c84a9562e2
Jackson Doyle <jacksondoyle@Jacksons-Mac-mini.local>
2026-04-13 19:26:34 -0700

    gh-pages fix

--- task-2/vite.config.ts
+++ task-2/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/nucleus-internship/",
+  base: "/Nucleus-Internship/",
   plugins: [react(), tailwindcss()],
 })
